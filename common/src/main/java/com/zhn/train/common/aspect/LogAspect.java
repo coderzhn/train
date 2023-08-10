260f34e00a5a0f0b09a7ab93b884989f389fe3d9
ZhangHaonan <Hao-Nan.Zhang@pccw.com>
2023-08-10 22:06:27 +0800

    增加公共模块common用于放置公共的类，例如:工具类、拦截器、AOP、常量、枚举、公共配置等

--- common/src/main/java/com/zhn/train/common/aspect/LogAspect.java
+++ common/src/main/java/com/zhn/train/common/aspect/LogAspect.java
@@ -1,4 +1,4 @@
-package com.zhn.train.member.aspect;
+package com.zhn.train.common.aspect;
 
 import com.alibaba.fastjson.JSONObject;
 import com.alibaba.fastjson.support.spring.PropertyPreFilters;
@@ -23,7 +23,7 @@ import org.springframework.web.multipart.MultipartFile;
 @Component
 public class LogAspect {
     public LogAspect() {
-        System.out.println("LogAspect");
+        System.out.println("Common LogAspect");
     }
 
     private final static Logger LOG = LoggerFactory.getLogger(LogAspect.class);
