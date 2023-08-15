a851ba68e75435b7effdd01b8b5c1242aa026852
ZhangHaonan <Hao-Nan.Zhang@pccw.com>
2023-08-15 15:47:51 +0800

    使用hutool生成JWT token

--- member/src/main/java/com/zhn/train/member/service/MemberService.java
+++ member/src/main/java/com/zhn/train/member/service/MemberService.java
@@ -3,6 +3,7 @@ package com.zhn.train.member.service;
 import cn.hutool.core.bean.BeanUtil;
 import cn.hutool.core.collection.CollUtil;
 import cn.hutool.core.util.ObjectUtil;
+import cn.hutool.jwt.JWTUtil;
 import com.zhn.train.common.exception.BusinessException;
 import com.zhn.train.common.exception.BusinessExceptionEnum;
 import com.zhn.train.common.util.SnowUtil;
@@ -19,6 +20,7 @@ import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Map;
 
 @Service
 public class MemberService {
@@ -79,7 +81,14 @@ public class MemberService {
         if(!"8888".equals(code)){
             throw new BusinessException(BusinessExceptionEnum.MEMBER_MOBILE_CODE_ERROR);
         }
-        return BeanUtil.copyProperties(memberDB,MemberLoginResp.class);
+
+        MemberLoginResp memberLoginResp = BeanUtil.copyProperties(memberDB, MemberLoginResp.class);
+        Map<String, Object> map = BeanUtil.beanToMap(memberLoginResp);
+        String key = "coder12306";
+        String token = JWTUtil.createToken(map, key.getBytes());
+        JWTUtil.createToken(map,key.getBytes());
+        memberLoginResp.setToken(token);
+        return memberLoginResp;
     }
 
     private Member selectByMobile(String mobile) {
