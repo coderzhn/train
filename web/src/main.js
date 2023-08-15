ec26d64939919a332b488ff9c16133725efc8b17
ZhangHaonan <Hao-Nan.Zhang@pccw.com>
2023-08-15 14:12:29 +0800

    增加axios拦截器，打印请求参数和返回结果

--- web/src/main.js
+++ web/src/main.js
@@ -5,6 +5,7 @@ import store from './store'
 import Antd from 'ant-design-vue';
 import 'ant-design-vue/dist/antd.css';
 import * as Icons from '@ant-design/icons-vue'
+import axios from'axios';
 
 const app = createApp(App)
 
@@ -13,3 +14,17 @@ const icons = Icons;
 for(const i in icons){
     app.component(i,icons[i]);
 }
+axios.interceptors.request.use(function (config){
+    console.log('请求参数',config);
+    return config;
+},error => {
+    return Promise.reject(error)
+});
+
+axios.interceptors.response.use(function (response) {
+    console.log('返回结果：', response);
+    return response;
+}, error => {
+    console.log('返回错误：', error);
+    return Promise.reject(error);
+});
