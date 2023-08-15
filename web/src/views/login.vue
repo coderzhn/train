5a6be9ad8d460d95e75a2da6bdfbb006b9521a42
ZhangHaonan <Hao-Nan.Zhang@pccw.com>
2023-08-15 12:12:21 +0800

    集成axios，npm install axios。发起后端请求会报跨域访问错误

--- web/src/views/login.vue
+++ web/src/views/login.vue
@@ -39,6 +39,8 @@
 
 <script>
 import { defineComponent, reactive } from 'vue';
+import axios from'axios';
+
 export default defineComponent({
   name: "login-view",
   setup() {
@@ -47,16 +49,28 @@ export default defineComponent({
       code: '',
       remember: true,
     });
+
     const onFinish = values => {
       console.log('Success:', values);
     };
+
     const onFinishFailed = errorInfo => {
       console.log('Failed:', errorInfo);
     };
+
+    const sendCode = () => {
+        axios.post("http://localhost:8002/member/member/send-code",{
+         mobile: loginForm.mobile
+      }).then(response =>{
+            console.log(response);
+        });
+    };
+
     return {
       loginForm,
       onFinish,
       onFinishFailed,
+      sendCode,
     };
   },
 });
