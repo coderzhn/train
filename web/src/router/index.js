61bbef7efe1d74a5a0b86959ac52c079894f1342
ZhangHaonan <Hao-Nan.Zhang@pccw.com>
2023-08-17 16:05:08 +0800

    增加路由登录拦截，访问所有的控台页面都需要登陆

--- web/src/router/index.js
+++ web/src/router/index.js
@@ -1,5 +1,6 @@
 import { createRouter, createWebHistory } from 'vue-router'
-
+import store from "@/store";
+import {notification} from "ant-design-vue";
 
 const routes = [
   {
@@ -8,7 +9,10 @@ const routes = [
   },
   {
     path: "/",
-    component: () => import('../views/main.vue')
+    component: () => import('../views/main.vue'),
+    meta:{
+      loginRequire: true
+    }
   }
 ]
 
@@ -16,5 +20,26 @@ const router = createRouter({
   history: createWebHistory(process.env.BASE_URL),
   routes
 })
+
+// 路由登录拦截
+router.beforeEach((to, from, next) => {
+  // 要不要对meta.loginRequire属性做监控拦截
+  if (to.matched.some(function (item) {
+    console.log(item, "是否需要登录校验：", item.meta.loginRequire || false);
+    return item.meta.loginRequire
+  })) {
+    const _member = store.state.member;
+    console.log("页面登录校验开始：", _member);
+    if (!_member.token) {
+      console.log("用户未登录或登录超时！");
+      notification.error({ description: "未登录或登录超时" });
+      next('/login');
+    } else {
+      next();
+    }
+  } else {
+    next();
+  }
+});
 
 export default router
