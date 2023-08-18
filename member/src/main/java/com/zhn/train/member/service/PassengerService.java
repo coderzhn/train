e97585523fee657263ee89ef86435fcd6db8ccd1
ZhangHaonan <Hao-Nan.Zhang@pccw.com>
2023-08-18 17:59:37 +0800

    增加乘车人列表查询接口,只能查看当前会员自己添加的乘客

--- member/src/main/java/com/zhn/train/member/service/PassengerService.java
+++ member/src/main/java/com/zhn/train/member/service/PassengerService.java
@@ -2,14 +2,20 @@ package com.zhn.train.member.service;
 
 import cn.hutool.core.bean.BeanUtil;
 import cn.hutool.core.date.DateTime;
+import cn.hutool.core.util.ObjectUtil;
 import com.zhn.train.common.context.LoginMemberContext;
 import com.zhn.train.common.util.SnowUtil;
 import com.zhn.train.member.domain.Passenger;
+import com.zhn.train.member.domain.PassengerExample;
 import com.zhn.train.member.mapper.PassengerMapper;
+import com.zhn.train.member.req.PassengerQueryReq;
 import com.zhn.train.member.req.PassengerSaveReq;
+import com.zhn.train.member.resp.PassengerQueryResp;
 import jakarta.annotation.Resource;
 import jakarta.validation.Valid;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class PassengerService {
@@ -23,5 +29,14 @@ public class PassengerService {
         passenger.setCreateTime(now);
         passenger.setUpdateTime(now);
         passengerMapper.insert(passenger);
+    }
+    public List<PassengerQueryResp> queryList(@Valid PassengerQueryReq req){
+        PassengerExample passengerExample = new PassengerExample();
+        PassengerExample.Criteria criteria = passengerExample.createCriteria();
+        if (ObjectUtil.isNotNull(req.getMemberId())){
+            criteria.andMemberIdEqualTo(req.getMemberId());
+        }
+        List<Passenger> passengerList = passengerMapper.selectByExample(passengerExample);
+        return BeanUtil.copyToList(passengerList, PassengerQueryResp.class);
     }
 }
