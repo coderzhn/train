a8c7f07251dd934e30ab9ccf22d644054e84e0d4
ZhangHaonan <Hao-Nan.Zhang@pccw.com>
2023-08-18 17:34:38 +0800

    乘车人新增界面开发，增加乘车人表单,实现保存功能

--- web/src/views/main/passenger.vue
+++ web/src/views/main/passenger.vue
@@ -1,30 +1,64 @@
 <template>
   <a-button type="primary" @click="showModal">新增</a-button>
-  <a-modal v-model:visible="visible" title="乘车人" @ok="handleOK">
-    <p>Some contents...</p>
-    <p>Some contents...</p>
-    <p>Some contents...</p>
+  <a-modal v-model:visible="visible" title="乘车人" @ok="handleOk"
+           ok-text="确认" cancel-text="取消">
+    <a-form :model="passenger" :label-col="{span: 4}" :wrapper-col="{ span: 20 }">
+      <a-form-item label="会员id">
+        <a-input v-model:value="passenger.memberId" />
+      </a-form-item>
+      <a-form-item label="姓名">
+        <a-input v-model:value="passenger.name" />
+      </a-form-item>
+      <a-form-item label="身份证">
+        <a-input v-model:value="passenger.idCard" />
+      </a-form-item>
+      <a-form-item label="旅客类型">
+        <a-select v-model:value="passenger.type">
+          <a-select-option value="1">成人</a-select-option>
+          <a-select-option value="2">儿童</a-select-option>
+          <a-select-option value="3">学生</a-select-option>
+        </a-select>
+      </a-form-item>
+    </a-form>
   </a-modal>
 </template>
 <script>
-import {defineComponent,ref} from "vue";
+import {defineComponent,ref,reactive} from "vue";
+import {notification} from "ant-design-vue";
+import axios from "axios";
 
 export default defineComponent({
   setup(){
     const visible = ref(false);
-
+    const passenger = reactive({
+      id: undefined,
+      memberId: undefined,
+      name: undefined,
+      idCard: undefined,
+      type: undefined,
+      createTime: undefined,
+      updateTime: undefined,
+    })
     const showModal = () => {
       visible.value = true;
     };
 
-    const handleOK = e => {
-      console.log(e);
-      visible.value = false;
+    const handleOk = () => {
+      axios.post("/member/passenger/save", passenger).then((response) => {
+        let data = response.data;
+        if (data.success) {
+          notification.success({description: "保存成功！"});
+          visible.value = false;
+        } else {
+          notification.error({description: data.message});
+        }
+      });
     };
     return{
       visible,
       showModal,
-      handleOK,
+      handleOk,
+      passenger,
     };
   },
 });
