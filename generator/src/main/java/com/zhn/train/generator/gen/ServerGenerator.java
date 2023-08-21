496da05f1f88b0fdfe9231224d6102bc7775abb6
ZhangHaonan <Hao-Nan.Zhang@pccw.com>
2023-08-21 16:22:03 +0800

    增加DBUtil，获取表字段信息

--- generator/src/main/java/com/zhn/train/generator/gen/ServerGenerator.java
+++ generator/src/main/java/com/zhn/train/generator/gen/ServerGenerator.java
@@ -1,5 +1,6 @@
 package com.zhn.train.generator.gen;
 
+import com.zhn.train.generator.util.DbUtil;
 import com.zhn.train.generator.util.FreemarkerUtil;
 import freemarker.template.TemplateException;
 import org.dom4j.Document;
@@ -33,6 +34,18 @@ public class ServerGenerator {
         Node tableName = table.selectSingleNode("@tableName");
         Node domainObjectName = table.selectSingleNode("@domainObjectName");
         System.out.println(tableName.getText() + "/" + domainObjectName.getText());
+
+
+        // 为DbUtil设置数据源
+        Node connectionURL = document.selectSingleNode("//@connectionURL");
+        Node userId = document.selectSingleNode("//@userId");
+        Node password = document.selectSingleNode("//@password");
+        System.out.println("url: " + connectionURL.getText());
+        System.out.println("user: " + userId.getText());
+        System.out.println("password: " + password.getText());
+        DbUtil.url = connectionURL.getText();
+        DbUtil.user = userId.getText();
+        DbUtil.password = password.getText();
 
         // 示例：表名 jiawa_test
         // Domain = JiawaTest
