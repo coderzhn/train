1363bf64ca1fbd76278796f49f4f093669853044
ZhangHaonan <Hao-Nan.Zhang@pccw.com>
2023-08-21 11:51:41 +0800

    乘车人列表查询界面开发，保存成功后刷新列表

--- member/src/main/java/com/zhn/train/member/service/PassengerService.java
+++ member/src/main/java/com/zhn/train/member/service/PassengerService.java
@@ -38,6 +38,7 @@ public class PassengerService {
     }
     public PageResp<PassengerQueryResp> queryList(@Valid PassengerQueryReq req){
         PassengerExample passengerExample = new PassengerExample();
+        passengerExample.setOrderByClause("id desc");
         PassengerExample.Criteria criteria = passengerExample.createCriteria();
         if (ObjectUtil.isNotNull(req.getMemberId())){
             criteria.andMemberIdEqualTo(req.getMemberId());
