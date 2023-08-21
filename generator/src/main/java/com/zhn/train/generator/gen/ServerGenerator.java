cf751c05f6e9a4ea4c2f26596abf86f97442349d
ZhangHaonan <Hao-Nan.Zhang@pccw.com>
2023-08-21 17:13:27 +0800

    制作queryResp.ftl模板

--- generator/src/main/java/com/zhn/train/generator/gen/ServerGenerator.java
+++ generator/src/main/java/com/zhn/train/generator/gen/ServerGenerator.java
@@ -77,8 +77,8 @@ public class ServerGenerator {
 
         // gen(Domain, param, "service", "service");
         // gen(Domain, param, "controller/admin", "adminController");
-         gen(Domain, param, "req", "saveReq");
-        // gen(Domain, param, "req", "queryReq");
+         gen(Domain, param, "resp", "queryResp");
+//         gen(Domain, param, "req", "queryReq");
         // gen(Domain, param, "resp", "queryResp");
 
 //        genVue(do_main, param);
