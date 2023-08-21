15b1d163e96d586f53cde89042554933fcfd0afa
ZhangHaonan <Hao-Nan.Zhang@pccw.com>
2023-08-21 12:07:10 +0800

    乘车人编辑接口开发

--- member/src/main/java/com/zhn/train/member/service/PassengerService.java
+++ member/src/main/java/com/zhn/train/member/service/PassengerService.java
@@ -30,11 +30,16 @@ public class PassengerService {
     public void save(@Valid PassengerSaveReq req){
         DateTime now = DateTime.now();
         Passenger passenger = BeanUtil.copyProperties(req, Passenger.class);
-        passenger.setMemberId(LoginMemberContext.getId());
-        passenger.setId(SnowUtil.getSnowflakeNextId());
-        passenger.setCreateTime(now);
-        passenger.setUpdateTime(now);
-        passengerMapper.insert(passenger);
+        if (ObjectUtil.isNull(passenger.getId())){
+            passenger.setMemberId(LoginMemberContext.getId());
+            passenger.setId(SnowUtil.getSnowflakeNextId());
+            passenger.setCreateTime(now);
+            passenger.setUpdateTime(now);
+            passengerMapper.insert(passenger);
+        }else {
+            passenger.setUpdateTime(now);
+            passengerMapper.updateByPrimaryKey(passenger);
+        }
     }
     public PageResp<PassengerQueryResp> queryList(@Valid PassengerQueryReq req){
         PassengerExample passengerExample = new PassengerExample();
