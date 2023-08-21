f967326fb9e83529ee521232ebe67e9052d7fa5a
ZhangHaonan <Hao-Nan.Zhang@pccw.com>
2023-08-21 17:26:00 +0800

    使用vue.ftl模板生成的前端passenger文件

--- generator/src/main/java/com/zhn/train/generator/gen/ServerGenerator.java
+++ generator/src/main/java/com/zhn/train/generator/gen/ServerGenerator.java
@@ -14,7 +14,7 @@ import java.io.IOException;
 import java.util.*;
 
 public class ServerGenerator {
-    static boolean readOnly = true;
+    static boolean readOnly = false;
     static String vuePath = "admin/src/views/main/";
     static String serverPath = "[module]/src/main/java/com/zhn/train/[module]/";
     static String pomPath = "generator\\pom.xml";
