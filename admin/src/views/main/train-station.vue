b5075119db78e08c31bfeec08dfe638fa4ef3128
ZhangHaonan <Hao-Nan.Zhang@pccw.com>
2023-08-22 17:48:25 +0800

    火车车次下拉选择功能开发，前端调用后端接口成功

--- admin/src/views/main/train-station.vue
+++ admin/src/views/main/train-station.vue
@@ -216,11 +216,24 @@ export default defineComponent({
       });
     };
 
+    const queryTrainCode = () => {
+      axios.get("/business/admin/train/query-all").then((response) => {
+        let data = response.data;
+        if (data.success) {
+          console.log(data.content);
+        } else {
+          notification.error({description: data.message});
+        }
+      });
+    };
+
+
     onMounted(() => {
       handleQuery({
         page: 1,
         size: pagination.value.pageSize
       });
+      queryTrainCode();
     });
 
     return {
@@ -235,7 +248,8 @@ export default defineComponent({
       onAdd,
       handleOk,
       onEdit,
-      onDelete
+      onDelete,
+      queryTrainCode,
     };
   },
 });
