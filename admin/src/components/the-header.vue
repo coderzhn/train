339de661e83b785eb3858328eb2344c022934e14
ZhangHaonan <Hao-Nan.Zhang@pccw.com>
2023-08-22 09:35:50 +0800

    创建admin模块，所有的代码都是从web复制过来的

---
+++ admin/src/components/the-header.vue
@@ -0,0 +1,64 @@
+<template>
+  <a-layout-header class="header">
+    <div class="logo">
+    </div>
+    <div style="float: right; color: white;">
+      您好：{{member.mobile}} &nbsp;&nbsp;
+      <router-link to="/login" style="color: white;">
+        退出登录
+      </router-link>
+    </div>
+    <a-menu
+        v-model:selectedKeys="selectedKeys"
+        theme="dark"
+        mode="horizontal"
+        :style="{ lineHeight: '64px' }"
+    >
+      <a-menu-item key="/welcome">
+        <router-link to="/welcome">
+          <coffee-outlined /> &nbsp; 欢迎
+        </router-link>
+      </a-menu-item>
+      <a-menu-item key="/passenger">
+        <router-link to="/passenger">
+          <user-outlined /> &nbsp; 乘车人管理
+        </router-link>
+      </a-menu-item>
+    </a-menu>
+  </a-layout-header>
+</template>
+
+<script>
+import {defineComponent, ref, watch} from 'vue';
+import store from "@/store";
+import router from '@/router'
+
+export default defineComponent({
+  name: "the-header-view",
+  setup() {
+    let member = store.state.member;
+    const selectedKeys = ref([]);
+
+    watch(() => router.currentRoute.value.path, (newValue) => {
+      console.log('watch', newValue);
+      selectedKeys.value = [];
+      selectedKeys.value.push(newValue);
+    }, {immediate: true});
+    return {
+      member,
+      selectedKeys
+    };
+  },
+});
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.logo {
+  float: left;
+  height: 31px;
+  width: 150px;
+  color: white;
+  font-size: 20px;
+}
+</style>
