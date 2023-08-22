bea63db477e846c7b29de1b454e12f20981d7ce0
ZhangHaonan <Hao-Nan.Zhang@pccw.com>
2023-08-22 15:37:10 +0800

    增加车站表，使用生成器 生成持久层代码及前后端代码

---
+++ business/src/main/java/com/zhn/train/business/controller/StationController.java
@@ -0,0 +1,39 @@
+package com.zhn.train.business.controller;
+
+import com.zhn.train.common.context.LoginMemberContext;
+import com.zhn.train.common.resp.CommonResp;
+import com.zhn.train.common.resp.PageResp;
+import com.zhn.train.business.req.StationQueryReq;
+import com.zhn.train.business.req.StationSaveReq;
+import com.zhn.train.business.resp.StationQueryResp;
+import com.zhn.train.business.service.StationService;
+import jakarta.annotation.Resource;
+import jakarta.validation.Valid;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/admin/station")
+public class StationController {
+
+    @Resource
+    private StationService stationService;
+
+    @PostMapping("/save")
+    public CommonResp<Object> save(@Valid @RequestBody StationSaveReq req) {
+        stationService.save(req);
+        return new CommonResp<>();
+    }
+
+    @GetMapping("/query-list")
+    public CommonResp<PageResp<StationQueryResp>> queryList(@Valid StationQueryReq req) {
+        PageResp<StationQueryResp> list = stationService.queryList(req);
+        return new CommonResp<>(list);
+    }
+
+    @DeleteMapping("/delete/{id}")
+    public CommonResp<Object> delete(@PathVariable Long id) {
+        stationService.delete(id);
+        return new CommonResp<>();
+    }
+
+}
---
+++ business/src/main/java/com/zhn/train/business/service/StationService.java
@@ -0,0 +1,70 @@
+package com.zhn.train.business.service;
+
+import cn.hutool.core.bean.BeanUtil;
+import cn.hutool.core.date.DateTime;
+import cn.hutool.core.util.ObjectUtil;
+import com.github.pagehelper.PageHelper;
+import com.github.pagehelper.PageInfo;
+import com.zhn.train.common.resp.PageResp;
+import com.zhn.train.common.util.SnowUtil;
+import com.zhn.train.business.domain.Station;
+import com.zhn.train.business.domain.StationExample;
+import com.zhn.train.business.mapper.StationMapper;
+import com.zhn.train.business.req.StationQueryReq;
+import com.zhn.train.business.req.StationSaveReq;
+import com.zhn.train.business.resp.StationQueryResp;
+import jakarta.annotation.Resource;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class StationService {
+
+    private static final Logger LOG = LoggerFactory.getLogger(StationService.class);
+
+    @Resource
+    private StationMapper stationMapper;
+
+    public void save(StationSaveReq req) {
+        DateTime now = DateTime.now();
+        Station station = BeanUtil.copyProperties(req, Station.class);
+        if (ObjectUtil.isNull(station.getId())) {
+            station.setId(SnowUtil.getSnowflakeNextId());
+            station.setCreateTime(now);
+            station.setUpdateTime(now);
+            stationMapper.insert(station);
+        } else {
+            station.setUpdateTime(now);
+            stationMapper.updateByPrimaryKey(station);
+        }
+    }
+
+    public PageResp<StationQueryResp> queryList(StationQueryReq req) {
+        StationExample stationExample = new StationExample();
+        stationExample.setOrderByClause("id desc");
+        StationExample.Criteria criteria = stationExample.createCriteria();
+
+        LOG.info("查询页码：{}", req.getPage());
+        LOG.info("每页条数：{}", req.getSize());
+        PageHelper.startPage(req.getPage(), req.getSize());
+        List<Station> stationList = stationMapper.selectByExample(stationExample);
+
+        PageInfo<Station> pageInfo = new PageInfo<>(stationList);
+        LOG.info("总行数：{}", pageInfo.getTotal());
+        LOG.info("总页数：{}", pageInfo.getPages());
+
+        List<StationQueryResp> list = BeanUtil.copyToList(stationList, StationQueryResp.class);
+
+        PageResp<StationQueryResp> pageResp = new PageResp<>();
+        pageResp.setTotal(pageInfo.getTotal());
+        pageResp.setList(list);
+        return pageResp;
+    }
+
+    public void delete(Long id) {
+        stationMapper.deleteByPrimaryKey(id);
+    }
+}
