4c9013e3d414e0c9dbf5ed3ae6797d326b515c54
ZhangHaonan <Hao-Nan.Zhang@pccw.com>
2023-08-22 17:33:42 +0800

    火车车次下拉选择功能开发，增加后端接口

--- business/src/main/java/com/zhn/train/business/controller/admin/TrainAdminController.java
+++ business/src/main/java/com/zhn/train/business/controller/admin/TrainAdminController.java
@@ -1,15 +1,16 @@
 package com.zhn.train.business.controller.admin;
 
-import com.zhn.train.common.context.LoginMemberContext;
-import com.zhn.train.common.resp.CommonResp;
-import com.zhn.train.common.resp.PageResp;
 import com.zhn.train.business.req.TrainQueryReq;
 import com.zhn.train.business.req.TrainSaveReq;
 import com.zhn.train.business.resp.TrainQueryResp;
 import com.zhn.train.business.service.TrainService;
+import com.zhn.train.common.resp.CommonResp;
+import com.zhn.train.common.resp.PageResp;
 import jakarta.annotation.Resource;
 import jakarta.validation.Valid;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/admin/train")
@@ -27,6 +28,11 @@ public class TrainAdminController {
     @GetMapping("/query-list")
     public CommonResp<PageResp<TrainQueryResp>> queryList(@Valid TrainQueryReq req) {
         PageResp<TrainQueryResp> list = trainService.queryList(req);
+        return new CommonResp<>(list);
+    }
+    @GetMapping("/query-all")
+    public CommonResp<List<TrainQueryResp>> queryAll(@Valid TrainQueryReq req) {
+        List<TrainQueryResp> list = trainService.queryAll();
         return new CommonResp<>(list);
     }
 
