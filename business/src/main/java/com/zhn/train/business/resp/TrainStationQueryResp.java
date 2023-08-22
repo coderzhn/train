0ac1dd10965ad1390a2e289be4f7dde397498523
ZhangHaonan <Hao-Nan.Zhang@pccw.com>
2023-08-22 16:28:07 +0800

    增加火车车站表，生成持久层代码及前后端代码

---
+++ business/src/main/java/com/zhn/train/business/resp/TrainStationQueryResp.java
@@ -0,0 +1,93 @@
+package com.zhn.train.business.resp;
+
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
+import com.fasterxml.jackson.databind.ser.std.ToStringSerializer;
+import java.util.Date;
+import com.fasterxml.jackson.annotation.JsonFormat;
+import java.math.BigDecimal;
+import lombok.Data;
+@Data
+public class TrainStationQueryResp {
+
+    /**
+     * id
+     */
+    @JsonSerialize(using= ToStringSerializer.class)
+    private Long id;
+
+    /**
+     * 车次编号
+     */
+    private String trainCode;
+
+    /**
+     * 站序
+     */
+    private Integer index;
+
+    /**
+     * 站名
+     */
+    private String name;
+
+    /**
+     * 站名拼音
+     */
+    private String namePinyin;
+
+    /**
+     * 进站时间
+     */
+    @JsonFormat(pattern = "HH:mm:ss",timezone = "GMT+8")
+    private Date inTime;
+
+    /**
+     * 出站时间
+     */
+    @JsonFormat(pattern = "HH:mm:ss",timezone = "GMT+8")
+    private Date outTime;
+
+    /**
+     * 停站时长
+     */
+    @JsonFormat(pattern = "HH:mm:ss",timezone = "GMT+8")
+    private Date stopTime;
+
+    /**
+     * 里程（公里）|从上一站到本站的距离
+     */
+    private BigDecimal km;
+
+    /**
+     * 新增时间
+     */
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss",timezone = "GMT+8")
+    private Date createTime;
+
+    /**
+     * 修改时间
+     */
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss",timezone = "GMT+8")
+    private Date updateTime;
+
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        sb.append(getClass().getSimpleName());
+        sb.append(" [");
+        sb.append("Hash = ").append(hashCode());
+        sb.append(", id=").append(id);
+        sb.append(", trainCode=").append(trainCode);
+        sb.append(", index=").append(index);
+        sb.append(", name=").append(name);
+        sb.append(", namePinyin=").append(namePinyin);
+        sb.append(", inTime=").append(inTime);
+        sb.append(", outTime=").append(outTime);
+        sb.append(", stopTime=").append(stopTime);
+        sb.append(", km=").append(km);
+        sb.append(", createTime=").append(createTime);
+        sb.append(", updateTime=").append(updateTime);
+        sb.append("]");
+        return sb.toString();
+    }
+}
---
+++ business/src/main/java/com/zhn/train/business/service/TrainStationService.java
@@ -0,0 +1,70 @@
+package com.zhn.train.business.service;
+
+import cn.hutool.core.bean.BeanUtil;
+import cn.hutool.core.date.DateTime;
+import cn.hutool.core.util.ObjectUtil;
+import com.github.pagehelper.PageHelper;
+import com.github.pagehelper.PageInfo;
+import com.zhn.train.common.resp.PageResp;
+import com.zhn.train.common.util.SnowUtil;
+import com.zhn.train.business.domain.TrainStation;
+import com.zhn.train.business.domain.TrainStationExample;
+import com.zhn.train.business.mapper.TrainStationMapper;
+import com.zhn.train.business.req.TrainStationQueryReq;
+import com.zhn.train.business.req.TrainStationSaveReq;
+import com.zhn.train.business.resp.TrainStationQueryResp;
+import jakarta.annotation.Resource;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class TrainStationService {
+
+    private static final Logger LOG = LoggerFactory.getLogger(TrainStationService.class);
+
+    @Resource
+    private TrainStationMapper trainStationMapper;
+
+    public void save(TrainStationSaveReq req) {
+        DateTime now = DateTime.now();
+        TrainStation trainStation = BeanUtil.copyProperties(req, TrainStation.class);
+        if (ObjectUtil.isNull(trainStation.getId())) {
+            trainStation.setId(SnowUtil.getSnowflakeNextId());
+            trainStation.setCreateTime(now);
+            trainStation.setUpdateTime(now);
+            trainStationMapper.insert(trainStation);
+        } else {
+            trainStation.setUpdateTime(now);
+            trainStationMapper.updateByPrimaryKey(trainStation);
+        }
+    }
+
+    public PageResp<TrainStationQueryResp> queryList(TrainStationQueryReq req) {
+        TrainStationExample trainStationExample = new TrainStationExample();
+        trainStationExample.setOrderByClause("id desc");
+        TrainStationExample.Criteria criteria = trainStationExample.createCriteria();
+
+        LOG.info("查询页码：{}", req.getPage());
+        LOG.info("每页条数：{}", req.getSize());
+        PageHelper.startPage(req.getPage(), req.getSize());
+        List<TrainStation> trainStationList = trainStationMapper.selectByExample(trainStationExample);
+
+        PageInfo<TrainStation> pageInfo = new PageInfo<>(trainStationList);
+        LOG.info("总行数：{}", pageInfo.getTotal());
+        LOG.info("总页数：{}", pageInfo.getPages());
+
+        List<TrainStationQueryResp> list = BeanUtil.copyToList(trainStationList, TrainStationQueryResp.class);
+
+        PageResp<TrainStationQueryResp> pageResp = new PageResp<>();
+        pageResp.setTotal(pageInfo.getTotal());
+        pageResp.setList(list);
+        return pageResp;
+    }
+
+    public void delete(Long id) {
+        trainStationMapper.deleteByPrimaryKey(id);
+    }
+}
