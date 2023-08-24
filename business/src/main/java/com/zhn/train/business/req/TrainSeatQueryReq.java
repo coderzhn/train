3f257c32b5025aa5ffd1e8052d05ca99e62ab932
ZhangHaonan <Hao-Nan.Zhang@pccw.com>
2023-08-24 09:45:25 +0800

    为火车座位页面增加车次查询条件

--- business/src/main/java/com/zhn/train/business/req/TrainSeatQueryReq.java
+++ business/src/main/java/com/zhn/train/business/req/TrainSeatQueryReq.java
@@ -1,12 +1,17 @@
 package com.zhn.train.business.req;
 
 import com.zhn.train.common.req.PageReq;
+import lombok.Data;
 
+@Data
 public class TrainSeatQueryReq extends PageReq {
+
+    private String trainCode;
 
     @Override
     public String toString() {
         return "TrainSeatQueryReq{" +
+                "trainCode='" + trainCode + '\'' +
                 "} " + super.toString();
     }
 }
