0bce3853edf315daaccd60f8124ecbe35df915c0
ZhangHaonan <Hao-Nan.Zhang@pccw.com>
2023-08-24 10:29:40 +0800

    实现按车次生成座位功能，增加后端接口，测试成功

--- business/src/main/java/com/zhn/train/business/service/TrainSeatService.java
+++ business/src/main/java/com/zhn/train/business/service/TrainSeatService.java
@@ -3,20 +3,24 @@ package com.zhn.train.business.service;
 import cn.hutool.core.bean.BeanUtil;
 import cn.hutool.core.date.DateTime;
 import cn.hutool.core.util.ObjectUtil;
+import cn.hutool.core.util.StrUtil;
 import com.github.pagehelper.PageHelper;
 import com.github.pagehelper.PageInfo;
-import com.zhn.train.common.resp.PageResp;
-import com.zhn.train.common.util.SnowUtil;
+import com.zhn.train.business.domain.TrainCarriage;
 import com.zhn.train.business.domain.TrainSeat;
 import com.zhn.train.business.domain.TrainSeatExample;
+import com.zhn.train.business.enums.SeatColEnum;
 import com.zhn.train.business.mapper.TrainSeatMapper;
 import com.zhn.train.business.req.TrainSeatQueryReq;
 import com.zhn.train.business.req.TrainSeatSaveReq;
 import com.zhn.train.business.resp.TrainSeatQueryResp;
+import com.zhn.train.common.resp.PageResp;
+import com.zhn.train.common.util.SnowUtil;
 import jakarta.annotation.Resource;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
@@ -27,7 +31,8 @@ public class TrainSeatService {
 
     @Resource
     private TrainSeatMapper trainSeatMapper;
-
+    @Resource
+    private TrainCarriageService trainCarriageService;
     public void save(TrainSeatSaveReq req) {
         DateTime now = DateTime.now();
         TrainSeat trainSeat = BeanUtil.copyProperties(req, TrainSeat.class);
@@ -68,5 +73,46 @@ public class TrainSeatService {
 
     public void delete(Long id) {
         trainSeatMapper.deleteByPrimaryKey(id);
+    }
+    @Transactional
+    public void genTrainSeat(String trainCode){
+        DateTime now = DateTime.now();
+        //清空当前车次下的所有座位记录
+        TrainSeatExample seatExample = new TrainSeatExample();
+        TrainSeatExample.Criteria criteria = seatExample.createCriteria();
+        criteria.andTrainCodeEqualTo(trainCode);
+        trainSeatMapper.deleteByExample(seatExample);
+        //查找当前车次下的所有车厢
+        List<TrainCarriage> carriageList = trainCarriageService.selectByTrainCode(trainCode);
+        LOG.info("当前车次下的车厢数:{}",carriageList.size());
+        //循环生成每个车厢的座位
+        for(TrainCarriage trainCarriage:carriageList){
+            //拿到车厢数据
+            Integer rowCount = trainCarriage.getRowCount();
+            String seatType = trainCarriage.getSeatType();
+            int seatIndex = 1;
+            //根据座位类型，筛选出所有的列
+            List<SeatColEnum> colsByType = SeatColEnum.getColsByType(seatType);
+            LOG.info("当前车次下的车厢数:{}",colsByType.size());
+            //循环行数
+            for (int row = 1; row <= rowCount; row++) {
+                //循环列数
+                for(SeatColEnum seatColEnum:colsByType){
+                    //构造作为数据并保存
+                    TrainSeat trainSeat = new TrainSeat();
+                    trainSeat.setId(SnowUtil.getSnowflakeNextId());
+                    trainSeat.setTrainCode(trainCode);
+                    trainSeat.setCarriageIndex(trainCarriage.getIndex());
+                    trainSeat.setRow(StrUtil.fillBefore(String.valueOf(row),'0',2));
+                    trainSeat.setCol(seatColEnum.getCode());
+                    trainSeat.setSeatType(seatType);
+                    trainSeat.setCarriageSeatIndex(seatIndex++);
+                    trainSeat.setCreateTime(now);
+                    trainSeat.setUpdateTime(now);
+
+                    trainSeatMapper.insert(trainSeat);
+                }
+            }
+        }
     }
 }
