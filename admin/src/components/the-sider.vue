c7d38b515287d125f2e35325aaade4a50dee100b
ZhangHaonan <Hao-Nan.Zhang@pccw.com>
2023-08-25 15:32:55 +0800

    对菜单做分类，改为两级菜单;路由也改为两级菜单，修改vue文件位置，保持层级关系与路由一致

--- admin/src/components/the-sider.vue
+++ admin/src/components/the-sider.vue
@@ -2,6 +2,7 @@
   <a-layout-sider width="200" style="background: #fff">
     <a-menu
         v-model:selectedKeys="selectedKeys"
+        open-keys="['batch','base']"
         mode="inline"
         :style="{ height: '100%', borderRight: 0 }"
     >
@@ -15,31 +16,53 @@
           <user-outlined /> &nbsp; 关于
         </router-link>
       </a-menu-item>
-      <a-menu-item key="/station">
-        <router-link to="/station">
-          <user-outlined /> &nbsp; 车站管理
-        </router-link>
-      </a-menu-item>
-      <a-menu-item key="/train">
-        <router-link to="/train">
-          <user-outlined /> &nbsp; 火车管理
-        </router-link>
-      </a-menu-item>
-      <a-menu-item key="/train-station">
-        <router-link to="/train-station">
-          <user-outlined /> &nbsp; 火车车站
-        </router-link>
-      </a-menu-item>
-      <a-menu-item key="/train-carriage">
-        <router-link to="/train-carriage">
-          <user-outlined /> &nbsp; 火车车厢
-        </router-link>
-      </a-menu-item>
-      <a-menu-item key="/train-seat">
-        <router-link to="/train-seat">
-          <user-outlined /> &nbsp; 火车座位
-        </router-link>
-      </a-menu-item>
+      <a-sub-menu key="batch">
+        <template #title>
+          <span>
+            <UnorderedListOutlined />
+            跑批管理
+          </span>
+        </template>
+        <a-menu-item key="/batch/job">
+          <router-link to="/batch/job">
+            <user-outlined /> &nbsp; 任务管理
+          </router-link>
+        </a-menu-item>
+      </a-sub-menu>
+      <a-sub-menu key="base">
+        <template #title>
+          <span>
+            <UnorderedListOutlined />
+            基础数据
+          </span>
+        </template>
+        <a-menu-item key="/base/station">
+          <router-link to="/base/station">
+            <user-outlined /> &nbsp; 车站管理
+          </router-link>
+        </a-menu-item>
+        <a-menu-item key="/base/train">
+          <router-link to="/base/train">
+            <user-outlined /> &nbsp; 火车管理
+          </router-link>
+        </a-menu-item>
+        <a-menu-item key="/base/train-station">
+          <router-link to="/base/train-station">
+            <user-outlined /> &nbsp; 火车车站
+          </router-link>
+        </a-menu-item>
+        <a-menu-item key="/base/train-carriage">
+          <router-link to="/base/train-carriage">
+            <user-outlined /> &nbsp; 火车车厢
+          </router-link>
+        </a-menu-item>
+        <a-menu-item key="/base/train-seat">
+          <router-link to="/base/train-seat">
+            <user-outlined /> &nbsp; 火车座位
+          </router-link>
+        </a-menu-item>
+      </a-sub-menu>
+
     </a-menu>
   </a-layout-sider>
 </template>
--- admin/src/router/index.js
+++ admin/src/router/index.js
@@ -1,44 +1,43 @@
 import { createRouter, createWebHistory } from 'vue-router'
 
-const routes = [
-  {
-    path: '/login',
-    component: () => import('../views/login.vue')
-  },
-  {
-    path: "/",
-    component: () => import('../views/main.vue'),
-    meta:{
-      loginRequire: true
-    },
-    children:[{
-      path: 'welcome',
-      component: () => import('../views/main/welcome.vue')
-    },{
-      path: 'about',
-      component: () => import('../views/main/about.vue')
-    }, {
+const routes = [{
+  path: '/',
+  component: () => import('../views/main.vue'),
+  children: [{
+    path: 'welcome',
+    component: () => import('../views/main/welcome.vue'),
+  }, {
+    path: 'about',
+    component: () => import('../views/main/about.vue'),
+  }, {
+    path: 'base/',
+    children: [{
       path: 'station',
-      component: () => import('../views/main/station.vue')
+      component: () => import('../views/main/base/station.vue'),
     }, {
       path: 'train',
-      component: () => import('../views/main/train.vue')
+      component: () => import('../views/main/base/train.vue'),
     }, {
       path: 'train-station',
-      component: () => import('../views/main/train-station.vue')
+      component: () => import('../views/main/base/train-station.vue'),
     }, {
       path: 'train-carriage',
-      component: () => import('../views/main/train-carriage.vue')
+      component: () => import('../views/main/base/train-carriage.vue'),
     }, {
       path: 'train-seat',
-      component: () => import('../views/main/train-seat.vue')
+      component: () => import('../views/main/base/train-seat.vue'),
     }]
-  },
-  {
-    path:'',
-    redirect: '/welcome'
-  }
-]
+  }, {
+    path: 'batch/',
+    children: [{
+      path: 'job',
+      component: () => import('../views/main/batch/job.vue')
+    }]
+  }]
+}, {
+  path: '',
+  redirect: '/welcome'
+}];
 
 const router = createRouter({
   history: createWebHistory(process.env.BASE_URL),
