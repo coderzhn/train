9e87c993edb59c36d4def3a07b1c70cb470186b4
ZhangHaonan <Hao-Nan.Zhang@pccw.com>
2023-08-25 14:23:01 +0800

    禁止任务并发执行

--- batch/src/main/java/com/zhn/train/batch/job/SpringbootTestJob.java
+++ batch/src/main/java/com/zhn/train/batch/job/SpringbootTestJob.java
@@ -1,18 +1,18 @@
-package com.zhn.train.batch.job;
-
-import org.springframework.scheduling.annotation.EnableScheduling;
-import org.springframework.scheduling.annotation.Scheduled;
-import org.springframework.stereotype.Component;
-
-/*
-适合单体应用，不适合集群
-没法实时更改定时任务状态和策略
- */
-@Component
-@EnableScheduling
-public class SpringbootTestJob {
-    @Scheduled(cron = "0/5 * * * * ?")
-    private void test(){
-        System.out.println("**********************************");
-    }
-}
+//package com.zhn.train.batch.job;
+//
+//import org.springframework.scheduling.annotation.EnableScheduling;
+//import org.springframework.scheduling.annotation.Scheduled;
+//import org.springframework.stereotype.Component;
+//
+///*
+//适合单体应用，不适合集群
+//没法实时更改定时任务状态和策略
+// */
+//@Component
+//@EnableScheduling
+//public class SpringbootTestJob {
+//    @Scheduled(cron = "0/5 * * * * ?")
+//    private void test(){
+//        System.out.println("**********************************");
+//    }
+//}
--- batch/src/main/java/com/zhn/train/batch/job/TestJob.java
+++ batch/src/main/java/com/zhn/train/batch/job/TestJob.java
@@ -1,12 +1,21 @@
 package com.zhn.train.batch.job;
 
+import org.quartz.DisallowConcurrentExecution;
 import org.quartz.Job;
 import org.quartz.JobExecutionContext;
 import org.quartz.JobExecutionException;
 
+@DisallowConcurrentExecution
 public class TestJob implements Job {
     @Override
     public void execute(JobExecutionContext jobExecutionContext) throws JobExecutionException {
-        System.out.println("===========TestJob===========");
+        System.out.println("TestJob========>开始");
+        try {
+            Thread.sleep(3000);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+        System.out.println("TestJob========>结束");
     }
+
 }
