c3f03f1ba9f188a6ae9590caa0f7edfe28112018
ZhangHaonan <Hao-Nan.Zhang@pccw.com>
2023-08-28 10:39:11 +0800

    增加生成每日车次定时任务

---
+++ batch/src/main/java/com/zhn/train/batch/job/DailyTrainJob.java
@@ -0,0 +1,25 @@
+package com.zhn.train.batch.job;
+
+
+import cn.hutool.core.util.RandomUtil;
+import org.quartz.DisallowConcurrentExecution;
+import org.quartz.Job;
+import org.quartz.JobExecutionContext;
+import org.quartz.JobExecutionException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.slf4j.MDC;
+
+@DisallowConcurrentExecution
+public class DailyTrainJob implements Job {
+
+    private static final Logger LOG = LoggerFactory.getLogger(DailyTrainJob.class);
+
+
+    @Override
+    public void execute(JobExecutionContext jobExecutionContext) throws JobExecutionException {
+        MDC.put("LOG_ID", System.currentTimeMillis() + RandomUtil.randomString(3));
+        LOG.info("生成每日车次数据开始");
+        LOG.info("生成每日车次数据结束");
+    }
+}
