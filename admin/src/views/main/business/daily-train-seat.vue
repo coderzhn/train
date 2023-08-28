11f20015c91ec72c27bfd8337dfef0ffee8f1ac2
ZhangHaonan <Hao-Nan.Zhang@pccw.com>
2023-08-28 10:15:11 +0800

    修改每日座位界面，增加车次查询条件

--- admin/src/views/main/business/daily-train-seat.vue
+++ admin/src/views/main/business/daily-train-seat.vue
@@ -1,8 +1,9 @@
 <template>
   <p>
     <a-space>
-      <a-button type="primary" @click="handleQuery()">刷新</a-button>
-      <a-button type="primary" @click="onAdd">新增</a-button>
+      <a-date-picker v-model:value="params.date" valueFormat="YYYY-MM-DD" placeholder="请选择日期" />
+      <train-select-view v-model="params.trainCode" width="200px"></train-select-view>
+      <a-button type="primary" @click="handleQuery()">查询</a-button>
     </a-space>
   </p>
   <a-table :dataSource="dailyTrainSeats"
@@ -12,19 +13,10 @@
            :loading="loading">
     <template #bodyCell="{ column, record }">
       <template v-if="column.dataIndex === 'operation'">
-        <a-space>
-          <a-popconfirm
-              title="删除后不可恢复，确认删除?"
-              @confirm="onDelete(record)"
-              ok-text="确认" cancel-text="取消">
-            <a style="color: red">删除</a>
-          </a-popconfirm>
-          <a @click="onEdit(record)">编辑</a>
-        </a-space>
       </template>
       <template v-else-if="column.dataIndex === 'col'">
         <span v-for="item in SEAT_COL_ARRAY" :key="item.code">
-          <span v-if="item.code === record.col">
+          <span v-if="item.code === record.col && item.type === record.seatType">
             {{item.desc}}
           </span>
         </span>
@@ -38,52 +30,17 @@
       </template>
     </template>
   </a-table>
-  <a-modal v-model:visible="visible" title="每日座位" @ok="handleOk"
-           ok-text="确认" cancel-text="取消">
-    <a-form :model="dailyTrainSeat" :label-col="{span: 4}" :wrapper-col="{ span: 20 }">
-      <a-form-item label="日期">
-        <a-date-picker v-model:value="dailyTrainSeat.date" valueFormat="YYYY-MM-DD" placeholder="请选择日期" />
-      </a-form-item>
-      <a-form-item label="车次编号">
-        <a-input v-model:value="dailyTrainSeat.trainCode" />
-      </a-form-item>
-      <a-form-item label="箱序">
-        <a-input v-model:value="dailyTrainSeat.carriageIndex" />
-      </a-form-item>
-      <a-form-item label="排号">
-        <a-input v-model:value="dailyTrainSeat.row" />
-      </a-form-item>
-      <a-form-item label="列号">
-        <a-select v-model:value="dailyTrainSeat.col">
-          <a-select-option v-for="item in SEAT_COL_ARRAY" :key="item.code" :value="item.code">
-            {{item.desc}}
-          </a-select-option>
-        </a-select>
-      </a-form-item>
-      <a-form-item label="座位类型">
-        <a-select v-model:value="dailyTrainSeat.seatType">
-          <a-select-option v-for="item in SEAT_TYPE_ARRAY" :key="item.code" :value="item.code">
-            {{item.desc}}
-          </a-select-option>
-        </a-select>
-      </a-form-item>
-      <a-form-item label="同车箱座序">
-        <a-input v-model:value="dailyTrainSeat.carriageSeatIndex" />
-      </a-form-item>
-      <a-form-item label="售卖情况">
-        <a-input v-model:value="dailyTrainSeat.sell" />
-      </a-form-item>
-    </a-form>
-  </a-modal>
 </template>
 
 <script>
 import { defineComponent, ref, onMounted } from 'vue';
 import {notification} from "ant-design-vue";
 import axios from "axios";
+import TrainSelectView from "@/components/train-select";
 
 export default defineComponent({
   name: "daily-train-seat-view",
+  components: {TrainSelectView},
   setup() {
     const SEAT_COL_ARRAY = window.SEAT_COL_ARRAY;
     const SEAT_TYPE_ARRAY = window.SEAT_TYPE_ARRAY;
@@ -109,6 +66,10 @@ export default defineComponent({
       pageSize: 10,
     });
     let loading = ref(false);
+    let params = ref({
+      trainCode: null,
+      date:null
+    })
     const columns = [
     {
       title: '日期',
@@ -150,52 +111,8 @@ export default defineComponent({
       dataIndex: 'sell',
       key: 'sell',
     },
-    {
-      title: '操作',
-      dataIndex: 'operation'
-    }
     ];
 
-    const onAdd = () => {
-      dailyTrainSeat.value = {};
-      visible.value = true;
-    };
-
-    const onEdit = (record) => {
-      dailyTrainSeat.value = window.Tool.copy(record);
-      visible.value = true;
-    };
-
-    const onDelete = (record) => {
-      axios.delete("/business/admin/daily-train-seat/delete/" + record.id).then((response) => {
-        const data = response.data;
-        if (data.success) {
-          notification.success({description: "删除成功！"});
-          handleQuery({
-            page: pagination.value.current,
-            size: pagination.value.pageSize,
-          });
-        } else {
-          notification.error({description: data.message});
-        }
-      });
-    };
-
-    const handleOk = () => {
-      axios.post("/business/admin/daily-train-seat/save", dailyTrainSeat.value).then((response) => {
-        let data = response.data;
-        if (data.success) {
-          notification.success({description: "保存成功！"});
-          visible.value = false;
-          handleQuery({
-            page: pagination.value.current,
-            size: pagination.value.pageSize
-          });
-        } else {
-          notification.error({description: data.message});
-        }
-      });
-    };
 
     const handleQuery = (param) => {
       if (!param) {
@@ -208,7 +125,10 @@ export default defineComponent({
       axios.get("/business/admin/daily-train-seat/query-list", {
         params: {
           page: param.page,
-          size: param.size
+          size: param.size,
+          trainCode: params.value.trainCode,
+          date: params.value.date
+
         }
       }).then((response) => {
         loading.value = false;
@@ -250,10 +170,7 @@ export default defineComponent({
       handleTableChange,
       handleQuery,
       loading,
-      onAdd,
-      handleOk,
-      onEdit,
-      onDelete
+      params,
     };
   },
 });
--- generator/src/main/java/com/zhn/train/generator/gen/ServerGenerator.java
+++ generator/src/main/java/com/zhn/train/generator/gen/ServerGenerator.java
@@ -14,7 +14,7 @@ import java.io.IOException;
 import java.util.*;
 
 public class ServerGenerator {
-    static boolean readOnly = false;
+    static boolean readOnly = true;
     static String vuePath = "admin/src/views/main/";
     static String serverPath = "[module]/src/main/java/com/zhn/train/[module]/";
     static String pomPath = "generator\\pom.xml";
@@ -76,11 +76,11 @@ public class ServerGenerator {
         param.put("readOnly", readOnly);
         System.out.println("组装参数：" + param);
 
-         gen(Domain, param, "service", "service");
-         gen(Domain, param, "controller/admin", "adminController");
-         gen(Domain, param, "resp", "queryResp");
-         gen(Domain, param, "req", "queryReq");
-         gen(Domain, param, "req", "saveReq");
+//         gen(Domain, param, "service", "service");
+//         gen(Domain, param, "controller/admin", "adminController");
+//         gen(Domain, param, "resp", "queryResp");
+//         gen(Domain, param, "req", "queryReq");
+//         gen(Domain, param, "req", "saveReq");
          genVue(do_main, param);
     }
 
