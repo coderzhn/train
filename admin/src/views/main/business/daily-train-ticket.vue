6a7237b29a39dddfa4bd23890372768e4bff8551
ZhangHaonan <Hao-Nan.Zhang@pccw.com>
2023-08-30 08:05:56 +0800

    将余票信息改为只读，添加车票车站界面的分页选项

--- admin/src/views/main/business/daily-train-ticket.vue
+++ admin/src/views/main/business/daily-train-ticket.vue
@@ -2,7 +2,7 @@
   <p>
     <a-space>
       <a-button type="primary" @click="handleQuery()">刷新</a-button>
-      <a-button type="primary" @click="onAdd">新增</a-button>
+      
     </a-space>
   </p>
   <a-table :dataSource="dailyTrainTickets"
@@ -12,77 +12,9 @@
            :loading="loading">
     <template #bodyCell="{ column, record }">
       <template v-if="column.dataIndex === 'operation'">
-        <a-space>
-          <a-popconfirm
-              title="删除后不可恢复，确认删除?"
-              @confirm="onDelete(record)"
-              ok-text="确认" cancel-text="取消">
-            <a style="color: red">删除</a>
-          </a-popconfirm>
-          <a @click="onEdit(record)">编辑</a>
-        </a-space>
       </template>
     </template>
   </a-table>
-  <a-modal v-model:visible="visible" title="余票信息" @ok="handleOk"
-           ok-text="确认" cancel-text="取消">
-    <a-form :model="dailyTrainTicket" :label-col="{span: 4}" :wrapper-col="{ span: 20 }">
-      <a-form-item label="日期">
-        <a-date-picker v-model:value="dailyTrainTicket.date" valueFormat="YYYY-MM-DD" placeholder="请选择日期" />
-      </a-form-item>
-      <a-form-item label="车次编号">
-        <a-input v-model:value="dailyTrainTicket.trainCode" />
-      </a-form-item>
-      <a-form-item label="出发站">
-        <a-input v-model:value="dailyTrainTicket.start" />
-      </a-form-item>
-      <a-form-item label="出发站拼音">
-        <a-input v-model:value="dailyTrainTicket.startPinyin" />
-      </a-form-item>
-      <a-form-item label="出发时间">
-        <a-time-picker v-model:value="dailyTrainTicket.startTime" valueFormat="HH:mm:ss" placeholder="请选择时间" />
-      </a-form-item>
-      <a-form-item label="出发站序">
-        <a-input v-model:value="dailyTrainTicket.startIndex" />
-      </a-form-item>
-      <a-form-item label="到达站">
-        <a-input v-model:value="dailyTrainTicket.end" />
-      </a-form-item>
-      <a-form-item label="到达站拼音">
-        <a-input v-model:value="dailyTrainTicket.endPinyin" />
-      </a-form-item>
-      <a-form-item label="到站时间">
-        <a-time-picker v-model:value="dailyTrainTicket.endTime" valueFormat="HH:mm:ss" placeholder="请选择时间" />
-      </a-form-item>
-      <a-form-item label="到站站序">
-        <a-input v-model:value="dailyTrainTicket.endIndex" />
-      </a-form-item>
-      <a-form-item label="一等座余票">
-        <a-input v-model:value="dailyTrainTicket.ydz" />
-      </a-form-item>
-      <a-form-item label="一等座票价">
-        <a-input v-model:value="dailyTrainTicket.ydzPrice" />
-      </a-form-item>
-      <a-form-item label="二等座余票">
-        <a-input v-model:value="dailyTrainTicket.edz" />
-      </a-form-item>
-      <a-form-item label="二等座票价">
-        <a-input v-model:value="dailyTrainTicket.edzPrice" />
-      </a-form-item>
-      <a-form-item label="软卧余票">
-        <a-input v-model:value="dailyTrainTicket.rw" />
-      </a-form-item>
-      <a-form-item label="软卧票价">
-        <a-input v-model:value="dailyTrainTicket.rwPrice" />
-      </a-form-item>
-      <a-form-item label="硬卧余票">
-        <a-input v-model:value="dailyTrainTicket.yw" />
-      </a-form-item>
-      <a-form-item label="硬卧票价">
-        <a-input v-model:value="dailyTrainTicket.ywPrice" />
-      </a-form-item>
-    </a-form>
-  </a-modal>
 </template>
 
 <script>
@@ -216,52 +148,8 @@ export default defineComponent({
       dataIndex: 'ywPrice',
       key: 'ywPrice',
     },
-    {
-      title: '操作',
-      dataIndex: 'operation'
-    }
     ];
 
-    const onAdd = () => {
-      dailyTrainTicket.value = {};
-      visible.value = true;
-    };
-
-    const onEdit = (record) => {
-      dailyTrainTicket.value = window.Tool.copy(record);
-      visible.value = true;
-    };
-
-    const onDelete = (record) => {
-      axios.delete("/business/admin/daily-train-ticket/delete/" + record.id).then((response) => {
-        const data = response.data;
-        if (data.success) {
-          notification.success({description: "删除成功！"});
-          handleQuery({
-            page: pagination.value.current,
-            size: pagination.value.pageSize,
-          });
-        } else {
-          notification.error({description: data.message});
-        }
-      });
-    };
-
-    const handleOk = () => {
-      axios.post("/business/admin/daily-train-ticket/save", dailyTrainTicket.value).then((response) => {
-        let data = response.data;
-        if (data.success) {
-          notification.success({description: "保存成功！"});
-          visible.value = false;
-          handleQuery({
-            page: pagination.value.current,
-            size: pagination.value.pageSize
-          });
-        } else {
-          notification.error({description: data.message});
-        }
-      });
-    };
 
     const handleQuery = (param) => {
       if (!param) {
@@ -290,11 +178,12 @@ export default defineComponent({
       });
     };
 
-    const handleTableChange = (pagination) => {
+    const handleTableChange = (page) => {
       // console.log("看看自带的分页参数都有啥：" + pagination);
+      pagination.value.pageSize = page.pageSize;
       handleQuery({
-        page: pagination.current,
-        size: pagination.pageSize
+        page: page.current,
+        size: page.pageSize
       });
     };
 
@@ -314,10 +203,6 @@ export default defineComponent({
       handleTableChange,
       handleQuery,
       loading,
-      onAdd,
-      handleOk,
-      onEdit,
-      onDelete
     };
   },
 });
--- generator/src/main/java/com/zhn/train/generator/ftl/vue.ftl
+++ generator/src/main/java/com/zhn/train/generator/ftl/vue.ftl
@@ -184,11 +184,12 @@ export default defineComponent({
       });
     };
 
-    const handleTableChange = (pagination) => {
+    const handleTableChange = (page) => {
       // console.log("看看自带的分页参数都有啥：" + pagination);
+      pagination.value.pageSize = page.pageSize;
       handleQuery({
-        page: pagination.current,
-        size: pagination.pageSize
+        page: page.current,
+        size: page.pageSize
       });
     };
 
