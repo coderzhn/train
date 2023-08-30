fef2ee550224a9c66b0c77e1b311f1a92c5fb34b
ZhangHaonan <Hao-Nan.Zhang@pccw.com>
2023-08-30 15:21:14 +0800

    为余票界面添加查询条件，优化余票信息界面

--- business/src/main/java/com/zhn/train/business/req/DailyTrainTicketQueryReq.java
+++ business/src/main/java/com/zhn/train/business/req/DailyTrainTicketQueryReq.java
@@ -1,12 +1,26 @@
 package com.zhn.train.business.req;
 
 import com.zhn.train.common.req.PageReq;
+import lombok.Data;
+import org.springframework.format.annotation.DateTimeFormat;
 
+import java.util.Date;
+
+@Data
 public class DailyTrainTicketQueryReq extends PageReq {
+    private String trainCode;
+    private String start;
+    private String end;
+    @DateTimeFormat(pattern = "yyyy-MM-dd")
+    private Date date;
 
     @Override
     public String toString() {
         return "DailyTrainTicketQueryReq{" +
+                "trainCode='" + trainCode + '\'' +
+                ", start='" + start + '\'' +
+                ", end='" + end + '\'' +
+                ", date=" + date +
                 "} " + super.toString();
     }
 }
