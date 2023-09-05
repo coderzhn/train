e1f04c61a1ba0a2829680c04ffb74464892bca1c
ZhangHaonan <Hao-Nan.Zhang@pccw.com>
2023-09-05 10:57:06 +0800

    美化车次信息的显示

--- web/src/views/main/order.vue
+++ web/src/views/main/order.vue
@@ -1,5 +1,13 @@
 <template>
-  <div>{{ dailyTrainTicket }}</div>
+  <div class="order-train">
+  <span class="order-train-main">{{dailyTrainTicket.date}}</span>&nbsp;
+  <span class="order-train-main">{{dailyTrainTicket.trainCode}}</span>次&nbsp;
+  <span class="order-train-main">{{dailyTrainTicket.start}}</span>站
+  <span class="order-train-main">({{dailyTrainTicket.startTime}})</span>&nbsp;
+  <span class="order-train-main">——</span>&nbsp;
+  <span class="order-train-main">{{dailyTrainTicket.end}}</span>站
+  <span class="order-train-main">({{dailyTrainTicket.endTime}})</span>&nbsp;
+  </div>
 </template>
 
 <script>
