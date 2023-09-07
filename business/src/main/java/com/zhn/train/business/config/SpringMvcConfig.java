b6e66607125e80b740f5c2be4305a13b5c4b23dc
ZhangHaonan <Hao-Nan.Zhang@pccw.com>
2023-09-07 12:45:25 +0800

    查出余票记录，需要得到真实的库存，business增加会员拦截器，将会员信息放入线程本地变量

--- business/src/main/java/com/zhn/train/business/config/SpringMvcConfig.java
+++ business/src/main/java/com/zhn/train/business/config/SpringMvcConfig.java
@@ -2,6 +2,7 @@ package com.zhn.train.business.config;
 
 
 import com.zhn.train.common.interceptor.LogInterceptor;
+import com.zhn.train.common.interceptor.MemberInterceptor;
 import jakarta.annotation.Resource;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
@@ -12,10 +13,17 @@ public class SpringMvcConfig implements WebMvcConfigurer {
 
    @Resource
    LogInterceptor logInterceptor;
+   @Resource
+   MemberInterceptor memberInterceptor;
 
    @Override
    public void addInterceptors(InterceptorRegistry registry) {
        registry.addInterceptor(logInterceptor)
                .addPathPatterns("/**");
+       registry.addInterceptor(memberInterceptor)
+              .addPathPatterns("/**")
+              .excludePathPatterns(
+                      "/business/hello"
+              );
    }
 }
--- business/src/main/java/com/zhn/train/business/domain/ConfirmOrder.java
+++ business/src/main/java/com/zhn/train/business/domain/ConfirmOrder.java
@@ -1,7 +1,10 @@
 package com.zhn.train.business.domain;
 
+import lombok.Data;
+
 import java.util.Date;
 
+@Data
 public class ConfirmOrder {
     private Long id;
 
@@ -24,94 +27,6 @@ public class ConfirmOrder {
     private Date updateTime;
 
     private String tickets;
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public Long getMemberId() {
-        return memberId;
-    }
-
-    public void setMemberId(Long memberId) {
-        this.memberId = memberId;
-    }
-
-    public Date getDate() {
-        return date;
-    }
-
-    public void setDate(Date date) {
-        this.date = date;
-    }
-
-    public String getTrainCode() {
-        return trainCode;
-    }
-
-    public void setTrainCode(String trainCode) {
-        this.trainCode = trainCode;
-    }
-
-    public String getStart() {
-        return start;
-    }
-
-    public void setStart(String start) {
-        this.start = start;
-    }
-
-    public String getEnd() {
-        return end;
-    }
-
-    public void setEnd(String end) {
-        this.end = end;
-    }
-
-    public Long getDailyTrainTicketId() {
-        return dailyTrainTicketId;
-    }
-
-    public void setDailyTrainTicketId(Long dailyTrainTicketId) {
-        this.dailyTrainTicketId = dailyTrainTicketId;
-    }
-
-    public String getStatus() {
-        return status;
-    }
-
-    public void setStatus(String status) {
-        this.status = status;
-    }
-
-    public Date getCreateTime() {
-        return createTime;
-    }
-
-    public void setCreateTime(Date createTime) {
-        this.createTime = createTime;
-    }
-
-    public Date getUpdateTime() {
-        return updateTime;
-    }
-
-    public void setUpdateTime(Date updateTime) {
-        this.updateTime = updateTime;
-    }
-
-    public String getTickets() {
-        return tickets;
-    }
-
-    public void setTickets(String tickets) {
-        this.tickets = tickets;
-    }
 
     @Override
     public String toString() {
