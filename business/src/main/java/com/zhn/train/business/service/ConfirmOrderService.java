08297c43bdacc75f8c142c72c07a193c2c3afd7b
ZhangHaonan <Hao-Nan.Zhang@pccw.com>
2023-09-07 12:01:10 +0800

    保存确认订单表，状态初始

--- business/src/main/java/com/zhn/train/business/service/ConfirmOrderService.java
+++ business/src/main/java/com/zhn/train/business/service/ConfirmOrderService.java
@@ -3,16 +3,19 @@ package com.zhn.train.business.service;
 import cn.hutool.core.bean.BeanUtil;
 import cn.hutool.core.date.DateTime;
 import cn.hutool.core.util.ObjectUtil;
+import com.alibaba.fastjson.JSON;
 import com.github.pagehelper.PageHelper;
 import com.github.pagehelper.PageInfo;
-import com.zhn.train.business.req.ConfirmOrderDoReq;
-import com.zhn.train.common.resp.PageResp;
-import com.zhn.train.common.util.SnowUtil;
 import com.zhn.train.business.domain.ConfirmOrder;
 import com.zhn.train.business.domain.ConfirmOrderExample;
+import com.zhn.train.business.enums.ConfirmOrderStatusEnum;
 import com.zhn.train.business.mapper.ConfirmOrderMapper;
+import com.zhn.train.business.req.ConfirmOrderDoReq;
 import com.zhn.train.business.req.ConfirmOrderQueryReq;
 import com.zhn.train.business.resp.ConfirmOrderQueryResp;
+import com.zhn.train.common.context.LoginMemberContext;
+import com.zhn.train.common.resp.PageResp;
+import com.zhn.train.common.util.SnowUtil;
 import jakarta.annotation.Resource;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -71,6 +74,21 @@ public class ConfirmOrderService {
     public void doConfirm(ConfirmOrderDoReq req) {
         //TODO 省略业务数据校验
         //TODO 保存确认订单表，状态初始
+        ConfirmOrder confirmOrder = new ConfirmOrder();
+        DateTime now = DateTime.now();
+        confirmOrder.setId(SnowUtil.getSnowflakeNextId());
+        confirmOrder.setMemberId(LoginMemberContext.getId());
+        confirmOrder.setCreateTime(now);
+        confirmOrder.setUpdateTime(now);
+        confirmOrder.setDate(req.getDate());
+        confirmOrder.setTrainCode(req.getTrainCode());
+        confirmOrder.setStart(req.getStart());
+        confirmOrder.setEnd(req.getEnd());
+        confirmOrder.setDailyTrainTicketId(req.getDailyTrainTicketId());
+        confirmOrder.setStatus(ConfirmOrderStatusEnum.INIT.toString());
+        confirmOrder.setTickets(JSON.toJSONString(req.getTickets()));
+
+        confirmOrderMapper.insert(confirmOrder);
         //TODO 查出余票记录，需要得到真实的库存
         //TODO 扣减余票数量，并判断余票是否重组
         //TODO 选座
