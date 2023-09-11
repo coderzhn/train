66f48e3ede4a0bcf8d06bc7eefb0ba1ea4d4f72c
ZhangHaonan <Hao-Nan.Zhang@pccw.com>
2023-09-11 17:52:03 +0800

    新增一个feign接口，保存车票信息

--- member/src/main/java/com/zhn/train/member/controller/admin/TicketAdminController.java
+++ member/src/main/java/com/zhn/train/member/controller/admin/TicketAdminController.java
@@ -1,10 +1,8 @@
 package com.zhn.train.member.controller.admin;
 
-import com.zhn.train.common.context.LoginMemberContext;
 import com.zhn.train.common.resp.CommonResp;
 import com.zhn.train.common.resp.PageResp;
 import com.zhn.train.member.req.TicketQueryReq;
-import com.zhn.train.member.req.TicketSaveReq;
 import com.zhn.train.member.resp.TicketQueryResp;
 import com.zhn.train.member.service.TicketService;
 import jakarta.annotation.Resource;
@@ -18,22 +16,10 @@ public class TicketAdminController {
     @Resource
     private TicketService ticketService;
 
-    @PostMapping("/save")
-    public CommonResp<Object> save(@Valid @RequestBody TicketSaveReq req) {
-        ticketService.save(req);
-        return new CommonResp<>();
-    }
-
     @GetMapping("/query-list")
     public CommonResp<PageResp<TicketQueryResp>> queryList(@Valid TicketQueryReq req) {
         PageResp<TicketQueryResp> list = ticketService.queryList(req);
         return new CommonResp<>(list);
-    }
-
-    @DeleteMapping("/delete/{id}")
-    public CommonResp<Object> delete(@PathVariable Long id) {
-        ticketService.delete(id);
-        return new CommonResp<>();
     }
 
 }
