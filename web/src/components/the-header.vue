85fd014d6b7c92fe79313ba7d38dc18e4c76c04e
ZhangHaonan <Hao-Nan.Zhang@pccw.com>
2023-09-12 09:27:43 +0800

    修改会员车票页面的显示 会员端增加我的车票页面

--- web/src/components/the-header.vue
+++ web/src/components/the-header.vue
@@ -32,6 +32,11 @@
           <user-outlined /> &nbsp; 余票查询
         </router-link>
       </a-menu-item>
+      <a-menu-item key="/my-ticket">
+        <router-link to="/my-ticket">
+          <user-outlined /> &nbsp; 我的车票
+        </router-link>
+      </a-menu-item>
     </a-menu>
   </a-layout-header>
 </template>
--- web/src/router/index.js
+++ web/src/router/index.js
@@ -25,6 +25,9 @@ const routes = [
     },{
       path: 'order',
       component: () => import('../views/main/order.vue')
+    },{
+      path: 'my-ticket',
+      component: () => import('../views/main/my-ticket.vue')
     }]
   },
   {
