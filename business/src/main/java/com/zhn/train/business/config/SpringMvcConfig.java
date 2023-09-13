5352d0b1a22a05070e10b1cbcaa6cbc5d0e6b727
ZhangHaonan <Hao-Nan.Zhang@pccw.com>
2023-09-13 11:48:21 +0800

    集成Nacos配置中心，实现动态刷新的功能

--- business/src/main/java/com/zhn/train/business/config/SpringMvcConfig.java
+++ business/src/main/java/com/zhn/train/business/config/SpringMvcConfig.java
@@ -8,7 +8,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-@Configuration
+@Configuration("BusinessConfiguration")
 public class SpringMvcConfig implements WebMvcConfigurer {
 
    @Resource
