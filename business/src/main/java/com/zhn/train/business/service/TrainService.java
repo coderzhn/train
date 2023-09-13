0228fe72cecb587ee72be30bf3a17cd42e149d1e
ZhangHaonan <Hao-Nan.Zhang@pccw.com>
2023-09-13 16:43:49 +0800

    演示关闭Mybatis事务的一级缓存

--- business/src/main/java/com/zhn/train/business/service/TrainService.java
+++ business/src/main/java/com/zhn/train/business/service/TrainService.java
@@ -20,6 +20,7 @@ import jakarta.annotation.Resource;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
@@ -70,6 +71,7 @@ public class TrainService {
         pageResp.setList(list);
         return pageResp;
     }
+    @Transactional
     public List<TrainQueryResp> queryAll() {
         List<Train> trainList = selectAll();
         return BeanUtil.copyToList(trainList, TrainQueryResp.class);
