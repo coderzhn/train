b7c4323d15eb7379144b1ad6eae95f5e15482572
ZhangHaonan <Hao-Nan.Zhang@pccw.com>
2023-09-13 16:22:54 +0800

    配置feign+nacos实现按应用名做远程调用

--- batch/src/main/java/com/zhn/train/batch/feign/BusinessFeign.java
+++ batch/src/main/java/com/zhn/train/batch/feign/BusinessFeign.java
@@ -7,11 +7,11 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import java.util.Date;
 
-@FeignClient(name = "business",url = "http://localhost:8002/business")
-//@FeignClient("business")
+//@FeignClient(name = "business",url = "http://localhost:8002/business")
+@FeignClient("business")
 public interface BusinessFeign {
-    @GetMapping("/hello")
+    @GetMapping("/business/hello")
     String hello();
-    @GetMapping("/admin/daily-train/gen-daily/{date}")
+    @GetMapping("/business/admin/daily-train/gen-daily/{date}")
     CommonResp<Object> genDaily(@PathVariable @DateTimeFormat(pattern = "yyyy-MM-dd") Date date);
 }
