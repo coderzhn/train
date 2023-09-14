cc4d6033073e958e5f351c762dfd3463ccd906d9
ZhangHaonan <Hao-Nan.Zhang@pccw.com>
2023-09-14 15:35:38 +0800

    spring boot集成redis

--- business/src/main/java/com/zhn/train/business/req/DailyTrainTicketQueryReq.java
+++ business/src/main/java/com/zhn/train/business/req/DailyTrainTicketQueryReq.java
@@ -5,6 +5,7 @@ import lombok.Data;
 import org.springframework.format.annotation.DateTimeFormat;
 
 import java.util.Date;
+import java.util.Objects;
 
 @Data
 public class DailyTrainTicketQueryReq extends PageReq {
@@ -22,5 +23,19 @@ public class DailyTrainTicketQueryReq extends PageReq {
                 ", end='" + end + '\'' +
                 ", date=" + date +
                 "} " + super.toString();
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        if (!super.equals(o)) return false;
+        DailyTrainTicketQueryReq that = (DailyTrainTicketQueryReq) o;
+        return Objects.equals(trainCode, that.trainCode) && Objects.equals(start, that.start) && Objects.equals(end, that.end) && Objects.equals(date, that.date);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(super.hashCode(), trainCode, start, end, date);
     }
 }
