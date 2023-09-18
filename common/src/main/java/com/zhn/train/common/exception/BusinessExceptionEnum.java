f6a936043382e45469bfa559e20ec2001a8ff8b8
ZhangHaonan <Hao-Nan.Zhang@pccw.com>
2023-09-18 23:32:47 +0800

    添加seata nacos集成事务管理 使用同步锁解决超卖问题 但是同步锁效率低，且多节点超卖问题仍旧存在 改进，使用redis实现分布式锁，解决问题

--- common/src/main/java/com/zhn/train/common/exception/BusinessExceptionEnum.java
+++ common/src/main/java/com/zhn/train/common/exception/BusinessExceptionEnum.java
@@ -9,7 +9,8 @@ public enum BusinessExceptionEnum {
     BUSINESS_TRAIN_STATION_INDEX_UNIQUE_ERROR("同车次站序已存在"),
     BUSINESS_TRAIN_CARRIAGE_INDEX_UNIQUE_ERROR("同车次箱序已存在"),
     CONFIRM_ORDER_TICKET_COUNT_ERROR("余票不足"),
-    CONFIRM_ORDER_EXCEPTION("服务器忙，请稍后重试");
+    CONFIRM_ORDER_EXCEPTION("服务器忙，请稍后重试"),
+    CONFIRM_ORDER_LOCK_FAIL("当前抢票人数多，请稍后重试");
     private String desc;
 
     BusinessExceptionEnum(String desc) {
