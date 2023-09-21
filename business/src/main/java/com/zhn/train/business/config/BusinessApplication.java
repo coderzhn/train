5cd418e0b213bfce6acfb09f290c22069c39d5eb
ZhangHaonan <Hao-Nan.Zhang@pccw.com>
2023-09-21 11:23:46 +0800

    使用sentinel限流

--- business/src/main/java/com/zhn/train/business/config/BusinessApplication.java
+++ business/src/main/java/com/zhn/train/business/config/BusinessApplication.java
@@ -1,5 +1,8 @@
 package com.zhn.train.business.config;
 
+import com.alibaba.csp.sentinel.slots.block.RuleConstant;
+import com.alibaba.csp.sentinel.slots.block.flow.FlowRule;
+import com.alibaba.csp.sentinel.slots.block.flow.FlowRuleManager;
 import org.mybatis.spring.annotation.MapperScan;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -9,6 +12,9 @@ import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.core.env.Environment;
+
+import java.util.ArrayList;
+import java.util.List;
 
 
 @SpringBootApplication
@@ -23,5 +29,18 @@ public class BusinessApplication {
         Environment env = app.run(args).getEnvironment();
         LOG.info("启动成功!");
         LOG.info("地址: \thttp://127.0.0.1:{}",env.getProperty("server.port"));
+        // // 限流规则
+         initFlowRules();
+         LOG.info("已定义限流规则");
+    }
+    private static void initFlowRules(){
+        List<FlowRule> rules = new ArrayList<>();
+        FlowRule rule = new FlowRule();
+        rule.setResource("doConfirm");
+        rule.setGrade(RuleConstant.FLOW_GRADE_QPS);
+        // Set limit QPS to 20.
+        rule.setCount(1);
+        rules.add(rule);
+        FlowRuleManager.loadRules(rules);
     }
 }
--- common/src/main/java/com/zhn/train/common/exception/BusinessExceptionEnum.java
+++ common/src/main/java/com/zhn/train/common/exception/BusinessExceptionEnum.java
@@ -10,7 +10,8 @@ public enum BusinessExceptionEnum {
     BUSINESS_TRAIN_CARRIAGE_INDEX_UNIQUE_ERROR("同车次箱序已存在"),
     CONFIRM_ORDER_TICKET_COUNT_ERROR("余票不足"),
     CONFIRM_ORDER_EXCEPTION("服务器忙，请稍后重试"),
-    CONFIRM_ORDER_LOCK_FAIL("当前抢票人数多，请稍后重试");
+    CONFIRM_ORDER_LOCK_FAIL("当前抢票人数多，请稍后重试"),
+    CONFIRM_ORDER_FLOW_EXCEPTION("当前抢票人数多，请稍后重试");
     private String desc;
 
     BusinessExceptionEnum(String desc) {
