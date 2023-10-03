48727754d42b572dbe0008c0ae41f842b95523c7
ZhangHaonan <Hao-Nan.Zhang@pccw.com>
2023-10-03 11:13:36 +0800

    类名有误，重新生成代码

--- business/src/main/java/com/zhn/train/business/req/SkTokenSaveReq.java
+++ business/src/main/java/com/zhn/train/business/req/SkTokenSaveReq.java
@@ -1,47 +1,44 @@
-package com.zhn.train.business.resp;
+package com.zhn.train.business.req;
 
-import com.fasterxml.jackson.databind.annotation.JsonSerialize;
-import com.fasterxml.jackson.databind.ser.std.ToStringSerializer;
 import java.util.Date;
 import com.fasterxml.jackson.annotation.JsonFormat;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import lombok.Data;
 @Data
-public class skTokenQueryResp {
+public class SkTokenSaveReq {
 
     /**
      * id
      */
-    @JsonSerialize(using= ToStringSerializer.class)
     private Long id;
-
     /**
      * 日期
      */
     @JsonFormat(pattern = "yyyy-MM-dd",timezone = "GMT+8")
+    @NotNull(message = "【日期】不能为空")
     private Date date;
-
     /**
      * 车次编号
      */
+    @NotBlank(message = "【车次编号】不能为空")
     private String trainCode;
-
     /**
      * 令牌余量
      */
+    @NotNull(message = "【令牌余量】不能为空")
     private Integer count;
-
     /**
      * 新增时间
      */
     @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss",timezone = "GMT+8")
     private Date createTime;
-
     /**
      * 修改时间
      */
     @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss",timezone = "GMT+8")
     private Date updateTime;
-
     @Override
     public String toString() {
         StringBuilder sb = new StringBuilder();
--- business/src/main/java/com/zhn/train/business/resp/SkTokenQueryResp.java
+++ business/src/main/java/com/zhn/train/business/resp/SkTokenQueryResp.java
@@ -1,24 +1,46 @@
-package com.zhn.train.business.domain;
+package com.zhn.train.business.resp;
 
-import lombok.Data;
-
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
+import com.fasterxml.jackson.databind.ser.std.ToStringSerializer;
 import java.util.Date;
-
+import com.fasterxml.jackson.annotation.JsonFormat;
+import lombok.Data;
 @Data
-public class skToken {
+public class SkTokenQueryResp {
+
+    /**
+     * id
+     */
+    @JsonSerialize(using= ToStringSerializer.class)
     private Long id;
 
+    /**
+     * 日期
+     */
+    @JsonFormat(pattern = "yyyy-MM-dd",timezone = "GMT+8")
     private Date date;
 
+    /**
+     * 车次编号
+     */
     private String trainCode;
 
+    /**
+     * 令牌余量
+     */
     private Integer count;
 
+    /**
+     * 新增时间
+     */
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss",timezone = "GMT+8")
     private Date createTime;
 
+    /**
+     * 修改时间
+     */
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss",timezone = "GMT+8")
     private Date updateTime;
-
-
 
     @Override
     public String toString() {
